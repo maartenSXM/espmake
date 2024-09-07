c53f457cdf489b869a545c45afbc1f636f306892
Maarten Koning <maarten@koning.ca>
2024-09-07 17:08:48 -0400

    Added defaults.yaml

--- example/config.h
+++ example/config.h
@@ -1,9 +1,6 @@
 #ifndef _INCLUDE_CONFIG_H_
 #define _INCLUDE_CONFIG_H_
 
-// common config
-#define CONFIG_HA			1	// Home Assistant?
-
 // Config specific to a project tag
 #ifdef ESPMAKE_PROJECT_lily
   #define CONFIG_DISPLAY		1	// touchscreen display?
@@ -11,7 +8,7 @@
  
 // Config specific to specific $USER
 #ifdef _USER_blort
-  #undef CONFIG_HA				// USER blort doesnt use HA
+  #define CONFIG_HA			1	// USER blort likes HA
 #endif // _USER_blort
 
 #endif // _INCLUDE_CONFIG_H_
